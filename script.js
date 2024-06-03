37a51b4e77a11452b907087dc397b9217b9173e8
ThailanInTallinn <thailangm@icloud.com>
2024-06-03 13:32:41 +0300

    Enables variable container side sizes

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 var divsContainer = document.querySelector(".divs-container");
-for (var i = 0; i < 256; i++) {
+var gridSide = Number(prompt("Insert side size"));
+var totalDivs = gridSide * gridSide;
+for (var i = 0; i < totalDivs; i++) {
     var div = document.createElement("div");
     div.setAttribute("class", "coloredDiv");
+    div.style.width = "".concat(300 / gridSide, "px");
+    div.style.height = "".concat(300 / gridSide, "px");
     divsContainer.appendChild(div);
 }
--- script.ts
+++ script.ts
@@ -1,6 +1,10 @@
 const divsContainer: HTMLElement = document.querySelector(".divs-container");
-for (let i: number = 0; i < 256; i++) {
+let gridSide: number = Number(prompt("Insert side size"));
+let totalDivs: number = gridSide * gridSide;
+for (let i: number = 0; i < totalDivs; i++) {
 	const div = document.createElement("div");
 	div.setAttribute("class", "coloredDiv");
+	div.style.width = `${300 / gridSide}px`;
+	div.style.height = `${300 / gridSide}px`;
 	divsContainer.appendChild(div);
 }

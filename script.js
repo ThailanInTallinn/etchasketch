a71a5ad04bbc33f0aff26d336905bab7832ddf3f
ThailanInTallinn <thailangm@icloud.com>
2024-06-03 11:50:18 +0300

    Fix: Generates 16 divs

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var divsContainer = document.querySelector(".divs-container");
 divsContainer.style.width = "1600px";
 divsContainer.style.height = "1600px";
-for (var i = 0; i <= 16; i++) {
+for (var i = 0; i <= 15; i++) {
     var div = document.createElement("div");
     div.setAttribute("class", "coloredDiv");
     div.style.width = "10%";

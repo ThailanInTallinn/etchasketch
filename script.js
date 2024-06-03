2b5d8546e9686016e2f12930a3c91c31175950a4
ThailanInTallinn <thailangm@icloud.com>
2024-06-03 13:07:50 +0300

    Changes loop to test a bigger number of divs

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 var divsContainer = document.querySelector(".divs-container");
-divsContainer.style.width = "1600px";
-divsContainer.style.height = "1600px";
-for (var i = 0; i <= 15; i++) {
+for (var i = 0; i < 256; i++) {
     var div = document.createElement("div");
     div.setAttribute("class", "coloredDiv");
-    div.style.width = "10%";
-    div.style.height = "10%";
-    div.style.backgroundColor = "blue";
     divsContainer.appendChild(div);
 }

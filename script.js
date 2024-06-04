f0786ce52f4a0ebb78c19752368f3199e1655252
ThailanInTallinn <thailangm@icloud.com>
2024-06-04 22:30:24 +0300

    Makes the chosen grid size to be rendered
    On the previous version, new pixel divs were being generated,
    regardless whether there were previous pixels inside the div.
    It was making the new rendered pixels to be placed below the
    parent div.

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
 var button = document.querySelector("button");
 var gridSide = 0;
+var divsContainer = document.querySelector(".divs-container");
 button.addEventListener("click", function () {
+    gridSide = 0;
+    var div = document.createElement("div");
     gridSide = Number(prompt("Insert the number of pixels"));
     if (gridSide > 100) {
         gridSide = 100;
     }
-});
-var divsContainer = document.querySelector(".divs-container");
-var totalDivs = gridSide * gridSide;
-for (var i = 0; i < totalDivs; i++) {
-    var div = document.createElement("div");
-    div.setAttribute("class", "coloredDiv");
-    div.style.width = "".concat(300 / gridSide, "px");
-    div.style.height = "".concat(300 / gridSide, "px");
-    divsContainer.appendChild(div);
-}
-divsContainer.addEventListener("mouseover", function (e) {
-    e.stopPropagation();
-    if (!e.target.classList.contains("divs-container")) {
-        var target = e.target;
-        target.style.backgroundColor = "rgb(".concat(Math.ceil(Math.random() * 256), " ").concat(Math.ceil(Math.random() * 256), " ").concat(Math.ceil(Math.random() * 256), ")");
+    var totalDivs = gridSide * gridSide;
+    if (divsContainer.childElementCount > 0) {
+        while (divsContainer.firstChild) {
+            divsContainer.firstChild.remove();
+        }
+        for (var i = 0; i < totalDivs; i++) {
+            div = document.createElement("div");
+            div.setAttribute("class", "coloredDiv");
+            div.style.width = "".concat(300 / gridSide, "px");
+            div.style.height = "".concat(300 / gridSide, "px");
+            divsContainer.appendChild(div);
+        }
     }
-    ;
+    else {
+        for (var i = 0; i < totalDivs; i++) {
+            div = document.createElement("div");
+            div.setAttribute("class", "coloredDiv");
+            div.style.width = "".concat(300 / gridSide, "px");
+            div.style.height = "".concat(300 / gridSide, "px");
+            divsContainer.appendChild(div);
+        }
+    }
+    divsContainer.addEventListener("mouseover", function (e) {
+        e.stopPropagation();
+        if (!e.target.classList.contains("divs-container")) {
+            var target = e.target;
+            target.style.backgroundColor = "rgb(".concat(Math.ceil(Math.random() * 256), " ").concat(Math.ceil(Math.random() * 256), " ").concat(Math.ceil(Math.random() * 256), ")");
+        }
+        ;
+    });
 });
--- script.ts
+++ script.ts
@@ -1,26 +1,44 @@
 const button: HTMLButtonElement = document.querySelector("button");
 let gridSide: number = 0;
+const divsContainer: HTMLElement = document.querySelector(".divs-container");
 button.addEventListener("click", () => {
+	gridSide = 0;
+	let div: HTMLDivElement = document.createElement("div");
 	gridSide = Number(prompt("Insert the number of pixels"));
 	if (gridSide > 100) {
 		gridSide = 100;
 	}
+	let totalDivs: number = gridSide * gridSide;
+	if (divsContainer.childElementCount > 0) {
+		while (divsContainer.firstChild) {
+			divsContainer.firstChild.remove();
+		}
+		for (let i: number = 0; i < totalDivs; i++) {
+			div = document.createElement("div");
+			div.setAttribute("class", "coloredDiv");
+			div.style.width = `${300 / gridSide}px`;
+			div.style.height = `${300 / gridSide}px`;
+			divsContainer.appendChild(div);
+		}
+
+	} else {
+		for (let i: number = 0; i < totalDivs; i++) {
+			div = document.createElement("div");
+			div.setAttribute("class", "coloredDiv");
+			div.style.width = `${300 / gridSide}px`;
+			div.style.height = `${300 / gridSide}px`;
+			divsContainer.appendChild(div);
+		}
+	}
+	divsContainer.addEventListener("mouseover", (e) => {
+		e.stopPropagation();
+		if (!(e.target as HTMLDivElement).classList.contains("divs-container")) {
+			const target = (e.target as HTMLDivElement);
+			target.style.backgroundColor = `rgb(${Math.ceil(Math.random() * 256)} ${Math.ceil(Math.random() * 256)} ${Math.ceil(Math.random() * 256)})`;
+
+		};
+	});
 })
-const divsContainer: HTMLElement = document.querySelector(".divs-container");
-let totalDivs: number = gridSide * gridSide;
-for (let i: number = 0; i < totalDivs; i++) {
-	const div = document.createElement("div");
-	div.setAttribute("class", "coloredDiv");
-	div.style.width = `${300 / gridSide}px`;
-	div.style.height = `${300 / gridSide}px`;
-	divsContainer.appendChild(div);
-}
 
-divsContainer.addEventListener("mouseover", (e) => {
-	e.stopPropagation();
-	if (!(e.target as HTMLDivElement).classList.contains("divs-container")) {
-		const target = (e.target as HTMLDivElement);
-		target.style.backgroundColor = `rgb(${Math.ceil(Math.random() * 256)} ${Math.ceil(Math.random() * 256)} ${Math.ceil(Math.random() * 256)})`;
 
-	};
-});
+
